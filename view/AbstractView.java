d0cfc6075f454abb3fa108d7f26c18bea7c4b450
ceciliaperana13 <cperana13@gmail.com>
2026-04-07 15:59:39 +0200

    javafx

--- view/AbstractView.java
+++ view/AbstractView.java
@@ -4,15 +4,13 @@ package view;
 import javax.swing.JFrame;
 
 public abstract class AbstractView extends JFrame {
-
     public AbstractView(String title) {
         setTitle(title);
-        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // centre la fenêtre
+        setLocationRelativeTo(null);
         setResizable(false);
     }
 
-    // méthode que chaque vue doit implémenter
     protected abstract void initComponents();
+
 }
