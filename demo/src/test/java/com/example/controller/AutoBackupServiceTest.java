626d30e961045a2d36eafbf23ea3e72a2b7f19b8
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-04-19 14:46:50 +0200

    fix : remove useless import

--- demo/src/test/java/com/example/controller/AutoBackupServiceTest.java
+++ demo/src/test/java/com/example/controller/AutoBackupServiceTest.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
